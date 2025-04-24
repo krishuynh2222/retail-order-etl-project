4c2d82168cd999d952441e643860e9f2224afa52
Ngan Huynh <146058636+krishuynh2222@users.noreply.github.com>
2025-04-24 02:02:02 -0400

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -1,3 +1,17 @@
+# Script Purpose: extract.py
+=================================================================================
+# This script loads raw CSV files from the `data/rawData/` directory into Pandas DataFrames.
+=================================================================================
+# It defines a reusable function `extract_all_data()` which returns a dictionary containing all raw tables:
+# - customers
+# - orders
+# - orderitems
+# - products
+# - payments
+=================================================================================
+# This script is typically the first step in the ETL pipeline and is imported by `transform.py` to begin processing data.
+=================================================================================
+
 import pandas as pd
 
 def extract_all_data():
