c3c6e2e6ecbde615715399bcdb8b24e8c6930d90
Ngan Huynh <146058636+krishuynh2222@users.noreply.github.com>
2025-04-28 15:30:18 -0400

    Update data_analysis_queries.sql

--- sql/data_analysis_queries.sql
+++ sql/data_analysis_queries.sql
@@ -9,37 +9,52 @@
 -- ============================================================================
 */
 
--- Top 10 best-selling products
-SELECT product_id, COUNT(*) AS total_sold
-FROM orderitems
-GROUP BY product_id
-ORDER BY total_sold DESC
+-- Top 10 best-selling product 
+SELECT 
+	oi.product_id,
+	p.product_category_name,
+	COUNT(oi.order_id) AS total_orders,
+	SUM(oi.price) AS total_revenue
+FROM orderitems oi
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY oi.product_id, p.product_category_name
+ORDER BY total_orders DESC
 LIMIT 10;
 
--- Top 5 Revenue-Generating Products
-SELECT product_id, ROUND(SUM(price), 2) AS total_revenue
-FROM orderitems
-GROUP BY product_id
+--  Top 5 Revenue-Generating Products
+SELECT 
+    p.product_category_name,
+    SUM(oi.price) AS total_revenue
+FROM orderitems oi
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY p.product_category_name
 ORDER BY total_revenue DESC
 LIMIT 5;
 
--- Sales by state
-SELECT customer_state, COUNT(*) AS total_orders
-FROM orders o
+-- Customer Orders by State
+SELECT 
+	c.customer_state,
+	COUNT(o.order_id) AS total_orders
+FROM orders o 
 JOIN customers c ON o.customer_id = c.customer_id
-GROUP BY customer_state
+GROUP BY c.customer_state
 ORDER BY total_orders DESC;
 
--- Product Category Performance
-SELECT product_category_name, COUNT(*) AS items_sold, ROUND(SUM(price), 2) AS revenue
-FROM orderitems oi
-JOIN products p ON oi.product_id = p.product_id
-GROUP BY product_category_name
-ORDER BY revenue DESC
-LIMIT 10;
-
---Payment Method Usage
-SELECT payment_type, COUNT(*) AS num_payments, SUM(payment_value) AS total_paid
+-- Payment Method Usage
+SELECT 
+	payment_type,
+	COUNT(*) AS payment_count,
+	SUM(payment_value) AS total_payment_value
 FROM payments
 GROUP BY payment_type
-ORDER BY total_paid DESC;
+ORDER BY payment_count DESC;
+
+--  Delivery Performance (Delays)
+SELECT
+	order_id,
+	order_purchase_timestamp,
+	order_delivered_timestamp,
+	DATE_PART('day', order_delivered_timestamp - order_purchase_timestamp) AS delivery_Days
+FROM orders
+WHERE order_delivered_timestamp IS NOT NULL
+ORDER BY delivery_days DESC;
